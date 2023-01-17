3039ee10d2b1ca3670ad37a5f35b8e606c8a0178
herbisaptikopti <herbisaptikopti@gmail.com>
2023-01-17 08:58:32 +0700

    file projet

---
+++ Project/matakuliah/hapus_matakuliah.php
@@ -0,0 +1,21 @@
+<?php
+include "../config.php";
+include "../database/koneksi.php";
+
+
+$id_matakuliah = isset($_GET['id_matakuliah']) ? $_GET['id_matakuliah'] : "";
+
+if ($id_matakuliah == "") {
+    echo "Tidak ada ID";
+} else {
+    $query_hapus = "DELETE FROM matakuliah WHERE id_matakuliah=".$id_matakuliah."";
+
+    $hapus = $koneksi->query($query_hapus);
+
+    if ($hapus) {
+        header("location: tampil_matakuliah.php");
+    } else {
+        echo "Gagal menghapus data";
+    }
+}
+?>
